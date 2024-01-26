05e598f194d93f9daf3673382c6a03226f148f1a
Fxrmillan <89488032+Fxrmillan@users.noreply.github.com>
2024-01-26 12:37:28 -0700

    removed unnecessary code from app.py

--- app.py
+++ app.py
@@ -5,38 +5,6 @@ from dash import html
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP],use_pages=True) #user pages = true allows it to be multipage IMPORTANT!
 
-
-def menueCardCreator(title,icon):
-    card = html.Div(
-        dbc.Card(
-            dbc.Row(
-                [
-                    dbc.Col(
-                        html.Img(
-                            src=app.get_asset_url(icon),
-                            className="img-fluid rounded-start",
-                            style={"width": "150px", "height": "100px"},
-                        ),
-                        className="col-md-4",
-                    ),
-                    dbc.Col(
-                        dbc.CardBody(
-                            [
-                                html.H5(title, className=title,style={"fontSize": "40px"}),
-                            ]
-                        ),
-                        className="col-md-8",
-                    ),
-                ],
-                className="g-0 d-flex align-items-center", justify="center"), #centers the card
-            className="w-75 mb-3 mx-auto clickable-card", #makes it clickable and sets its width
-            style={"height": "120px"}  # Adjust maxWidth and height for card size
-
-        )
-    )
-    return card
-
-
 navbar = dbc.NavbarSimple(
     children=[
         dbc.NavItem(dbc.NavLink("View Opened Project", href="#")),
@@ -51,14 +19,6 @@ navbar = dbc.NavbarSimple(
     color="primary",
     dark=True,
     className="fixed-top",
-)
-
-menueTitle =  html.Div(
-    dbc.Row(
-        dbc.Col(
-  style={"fontSize": "30px", "color": "white", "fontFamily": "Arial, sans-serif", "marginRight": "100px"} 
-         )
-    )
 )
 
 app.layout = html.Div([

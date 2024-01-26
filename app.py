56c62a2bb7656e325c166de04ab91f132c3d2167
UnstoppableCrown <jagonzalez63@miners.utep.edu>
2024-01-26 12:59:12 -0700

    Added redirects

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP],use_pages=True) #use
 navbar = dbc.NavbarSimple(
     children=[
         dbc.NavItem(dbc.NavLink("View Opened Project", href="#")),
-        dbc.NavItem(dbc.NavLink("Sync Projects", href="#")),
-        dbc.NavItem(dbc.NavLink("Manage Projects", href="#")),
-        dbc.NavItem(dbc.NavLink("Home", href="#")), #temporary replace with image
+        dbc.NavItem(dbc.NavLink("Sync Projects", href="/syncMenue")),
+        dbc.NavItem(dbc.NavLink("Manage Projects", href="/manageProjects")),
+        dbc.NavItem(dbc.NavLink("Home", href="/")), #temporary replace with image
 
 
     ],
